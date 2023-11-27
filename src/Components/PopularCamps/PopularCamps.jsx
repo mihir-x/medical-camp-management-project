1c7631965ac8335b38ecf5195861133dd569ea65
mihir-x <ictlancerx@gmail.com>
2023-11-27 23:46:02 +0600

    added contact us page and sort functionality in popular camp page

--- src/Components/PopularCamps/PopularCamps.jsx
+++ src/Components/PopularCamps/PopularCamps.jsx
@@ -2,33 +2,49 @@ import { useQuery } from "@tanstack/react-query";
 import SectionTitle from "../SectionTitle/SectionTitle";
 import Container from "../Shared/Container/Container";
 import useAxiosPublic from "../../Hooks/useAxiosPublic";
-import Loader from "../Loader/Loader";
 import CampCard from "../Shared/CampCard/CampCard";
 import useRole from "../../Hooks/useRole";
 import useUser from "../../Hooks/useUser";
 import { Link } from "react-router-dom";
 import { Button } from "flowbite-react";
+import { useEffect, useState } from "react";
 
 
 const PopularCamps = () => {
+    const [sorted, setSorted] = useState([])
     const axiosPublic = useAxiosPublic()
     const [userAccount] = useUser()
     const [role] = useRole()
-    const {data:camps, isLoading} = useQuery({
+    const { data: camps } = useQuery({
         queryKey: ['popular'],
-        queryFn: async() =>{
+        queryFn: async () => {
             const res = await axiosPublic.get('/camps/popular')
             return res.data
         }
     })
-    if(isLoading) return <Loader></Loader>
+    // if(isLoading) return <Loader></Loader>
+    const handleAsc = () => {
+        const asc = camps.slice().sort((a, b) => a.participant - b.participant)
+        setSorted(asc)
+    }
+    const handleDec = () => {
+        const dec = camps.slice().sort((a, b) => b.participant - a.participant)
+        setSorted(dec)
+    }
+    useEffect(() => {
+        setSorted(camps)
+    }, [camps])
     return (
         <Container>
             <div>
                 <SectionTitle heading='Popular Camps'></SectionTitle>
+                <div className="outline-2 flex flex-col md:flex-row gap-5 justify-end p-4">
+                    <Button pill onClick={handleAsc}>Sort By Ascending Order</Button>
+                    <Button pill onClick={handleDec}>Sort By Descending Order</Button>
+                </div>
                 <div className=" grid grid-cols-1 md:grid-cols-2 gap-8">
                     {
-                        camps?.map(camp => <CampCard key={camp._id} camp={camp} role={role} userAccount={userAccount}></CampCard>)
+                        sorted?.map(camp => <CampCard key={camp._id} camp={camp} role={role} userAccount={userAccount}></CampCard>)
                     }
                 </div>
                 <div className="flex justify-center mt-5 md:mt-10">
--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -1,13 +1,73 @@
 import { Helmet } from "react-helmet-async";
+import Container from "../../Components/Shared/Container/Container";
+import SectionTitle from "../../Components/SectionTitle/SectionTitle";
+import {  Button, Label, TextInput } from "flowbite-react";
+import Swal from "sweetalert2";
 
 
 const ContactUs = () => {
+
+    const handleContactUs = e =>{
+        e.preventDefault()
+        const form = e.target
+        const name = form.name.value
+        Swal.fire({
+            title: `Welcome ${name}. We will reach you shortly`,
+            showClass: {
+              popup: `
+                animate__animated
+                animate__fadeInUp
+                animate__faster
+              `
+            },
+            hideClass: {
+              popup: `
+                animate__animated
+                animate__fadeOutDown
+                animate__faster
+              `
+            }
+          });
+          form.reset()
+    }
+
     return (
         <div>
             <Helmet>
                 <title>MediVoyage | Contact Us</title>
             </Helmet>
-            This is contact us page 
+            <SectionTitle heading='Contact Us'></SectionTitle>
+            <Container>
+               
+                <div className="md:flex  justify-around items-center">
+                    <div>
+                        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                            Welcome to Medical Camp Management
+                        </h5>
+                        <p className="font-normal text-gray-700 dark:text-gray-400">
+                            Contact us and be part of an amazing journey. 
+                        </p>
+                    </div>
+                    <div>
+                        <form onSubmit={handleContactUs} className="flex flex-col gap-4">
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="name" value="Your Name" />
+                                </div>
+                                <TextInput id="name" name="name" type="text" required />
+                            </div>
+                            <div>
+                                <div className="mb-2 block">
+                                    <Label htmlFor="email1" value="Your email" />
+                                </div>
+                                <TextInput id="email1" name="email" type="email" placeholder="name@flowbite.com" required />
+                            </div>
+                            
+                            <Button type="submit">Submit</Button>
+                        </form>
+                    </div>
+                </div>
+            </Container>
         </div>
     );
 };
