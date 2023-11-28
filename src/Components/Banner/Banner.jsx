665219e9100d3a577817b511187f00e882827934
mihir-x <ictlancerx@gmail.com>
2023-11-28 21:16:49 +0600

    updated banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -4,9 +4,9 @@ import { useState } from "react";
 const Banner = () => {
     const [current, setCurrent] = useState(0)
     const bannerPhoto = [
-        'https://i.ibb.co/8N3XTTB/nsdvc9738i761.jpg',
-        'https://i.ibb.co/tYGSrC4/koenigsegg-gemera.jpg',
-        'https://i.ibb.co/nM5BVsm/teakak.jpg'
+        'https://i.ibb.co/n0VmMbC/Getty-Images-1214206519.jpg',
+        'https://i.ibb.co/zZCJkXW/images.jpg',
+        'https://i.ibb.co/j3W1h3R/6-Ways-of-Patient-Centered-Care-1.jpg'
     ]
     const length = bannerPhoto.length
     const handlePrev = () => {
@@ -17,10 +17,17 @@ const Banner = () => {
     }
 
     return (
-        <div className="h-[20rem] relative">
-            <button onClick={handlePrev} className="btn btn-primary rounded-full absolute left-5 top-[50%]">❮</button>
-            <img src={bannerPhoto[current]} alt="" className="h-full w-full" />
-            <button onClick={handleNext} className="btn btn-primary rounded-full absolute right-5 top-[50%]">❯</button>
+        <div className="h-[25rem] relative">
+            <div className="absolute z-10 h-full w-full bg-black opacity-50"></div>
+            <button onClick={handlePrev} className="btn btn-primary z-20 rounded-full absolute left-5 top-[50%]">❮</button>
+            <img src={bannerPhoto[current]} alt="" className="-z-20 h-full w-full" />
+            <div className="absolute z-50 top-1/3 left-[15%] md:left-[28%] text-center">
+                <h1 className=" text-lg md:text-3xl lg:text-5xl font-bold text-white">Welcome to MediVoyage</h1>
+                <h1 className=" text-base md:text-xl lg:text-3xl font-semibold text-white mt-5">30+ Successful Camps</h1>
+                <h1 className=" text-base md:text-xl lg:text-3xl font-semibold text-white">Served 1000+ Patients</h1>
+                
+            </div>
+            <button onClick={handleNext} className="btn btn-primary z-20 rounded-full absolute right-5 top-[50%]">❯</button>
         </div>
     );
 };
