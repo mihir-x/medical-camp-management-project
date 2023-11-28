08251a1ad49cf2c5ece649a34f29bd0ca5f3d637
mihir-x <ictlancerx@gmail.com>
2023-11-28 17:14:53 +0600

    added functionality to publish upcoming camp button

--- src/Pages/Dashboard/Organizers/ManageUpcoming/ManageUpcoming.jsx
+++ src/Pages/Dashboard/Organizers/ManageUpcoming/ManageUpcoming.jsx
@@ -22,93 +22,134 @@ const ManageUpcoming = () => {
         }
     })
     if (isLoading) return <Loader></Loader>
-    console.log(camps)
 
-    const handleCampDelete = async (id) => {
-        Swal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!"
-        }).then(async (result) => {
-            if (result.isConfirmed) {
-                try {
-                    const res = await axiosSecure.delete(`/${id}`)
-                    if (res.data.deletedCount > 0) {
-                        refetch()
-                        Swal.fire({
-                            title: "Deleted!",
-                            text: "Medical camp has been deleted.",
-                            icon: "success"
-                        });
-                    }
-                }
-                catch (err) {
-                    Swal.fire({
-                        icon: "error",
-                        title: "Oops...",
-                        text: err.message
-                    })
-                }
-
+    const handlePublish = async (data) => {
+        try {
+            const campData = {
+                name: data.name,
+                fee: parseInt(data.fee),
+                date: data.date,
+                time: data.time,
+                venue: data.venue,
+                services: data.services,
+                professionals: data.professionals[0],
+                audience: data.audience,
+                description: data.description,
+                photo: data.photo,
+                createdAt: Date.now(),
+                organizer: user?.email,
+                participant: 0
             }
-        });
+            const campRes = await axiosSecure.post('/camps', campData)
+            if (campRes.data.insertedId) {
+                const deleteUpcomingResult = await axiosSecure.delete(`/upcoming-camp/delete/${data._id}`)
+                if(deleteUpcomingResult.data.deletedCount>0){
+                    refetch()
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: `Camp published Successfully`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            }
+        
+        }
+        catch (err) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: err.message
+        })
     }
+}
 
-    const column = [
-        {
-            name: 'Name',
-            selector: row => row.title,
-        },
-        {
-            name: 'Date',
-            selector: row => row.date,
-        },
-        {
-            name: 'Professionals Count',
-            selector: row => row.professionals,
-        },
-        {
-            name: 'Participants Count',
-            selector: row => row.participants,
-        },
-        {
-            name: 'Publish',
-            selector: row => row.publish,
-        },
-        {
-            name: 'Update',
-            selector: row => row.update,
-        },
-        {
-            name: 'Delete',
-            selector: row => row.delete,
-        },
-    ]
+const handleCampDelete = async (id) => {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then(async (result) => {
+        if (result.isConfirmed) {
+            try {
+                const res = await axiosSecure.delete(`/upcoming-camp/delete/${id}`)
+                if (res.data.deletedCount > 0) {
+                    refetch()
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Medical camp has been deleted.",
+                        icon: "success"
+                    });
+                }
+            }
+            catch (err) {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: err.message
+                })
+            }
 
-    const tableData = camps?.map((camp) => ({
-        title: camp.name,
-        venue: camp.venue,
-        date: camp.date,
-        professionals: camp.interestedProfessional,
-        participants: camp.interestedParticipant,
-        publish: <Button disabled={camp?.interestedParticipant<2 || camp?.interestedProfessional<1} outline gradientDuoTone="pinkToOrange">Publish</Button>,
-        update: <Link to={`/dashboard/update-camp/${camp._id}`}><Button outline gradientDuoTone="purpleToBlue">Update</Button></Link>,
-        delete: <Button onClick={() => handleCampDelete(camp._id)} outline gradientDuoTone="pinkToOrange">Delete</Button>,
-    }))
+        }
+    });
+}
 
-    return (
-        <div className="mb-5 md:mb-8 lg:mb-16">
-            <Helmet>
-                <title>MediVoyage | Manage Upcoming Camps</title>
-            </Helmet>
-            <SectionTitle heading='Manage Upcoming Camps'></SectionTitle>
-            <DataTable columns={column} data={tableData} style={{ 'overflowX': 'auto' }}></DataTable>
-        </div>
-    );
+const column = [
+    {
+        name: 'Name',
+        selector: row => row.title,
+    },
+    {
+        name: 'Date',
+        selector: row => row.date,
+    },
+    {
+        name: 'Professionals Count',
+        selector: row => row.professionals,
+    },
+    {
+        name: 'Participants Count',
+        selector: row => row.participants,
+    },
+    {
+        name: 'Publish',
+        selector: row => row.publish,
+    },
+    {
+        name: 'Update',
+        selector: row => row.update,
+    },
+    {
+        name: 'Delete',
+        selector: row => row.delete,
+    },
+]
+
+const tableData = camps?.map((camp) => ({
+    title: camp.name,
+    venue: camp.venue,
+    date: camp.date,
+    professionals: camp.interestedProfessional,
+    participants: camp.interestedParticipant,
+    publish: <Button onClick={() => handlePublish(camp)} disabled={camp?.interestedParticipant < 2 || camp?.interestedProfessional < 1} outline gradientDuoTone="pinkToOrange">Publish</Button>,
+    update: <Link to={`/dashboard/update-camp/${camp._id}`}><Button outline gradientDuoTone="purpleToBlue">Update</Button></Link>,
+    delete: <Button onClick={() => handleCampDelete(camp._id)} outline gradientDuoTone="pinkToOrange">Delete</Button>,
+}))
+
+return (
+    <div className="mb-5 md:mb-8 lg:mb-16">
+        <Helmet>
+            <title>MediVoyage | Manage Upcoming Camps</title>
+        </Helmet>
+        <SectionTitle heading='Manage Upcoming Camps'></SectionTitle>
+        <DataTable columns={column} data={tableData} style={{ 'overflowX': 'auto' }}></DataTable>
+    </div>
+);
 };
 
 export default ManageUpcoming;
